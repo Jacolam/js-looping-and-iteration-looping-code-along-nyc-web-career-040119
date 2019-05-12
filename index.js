f77337c25f3eee48532575e2bcbe97309ad11282
Jacob Lam <jlam18@binghamton.edu>
2019-05-12 17:01:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, event){
+  let newArr = []
+  for (let i = 0; i < array.length; i++ ){
+    newArr.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+  }
+  return newArr
+}
+
+
+function countdown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--
+    }
+}
